fd85ad6cf8280d4b99d9857d6282b473799e5921
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-02-09 00:10:13 +0100

    Add currency & weather methods

--- models/currency.js
+++ models/currency.js
@@ -3,14 +3,50 @@
 
 'use strict'
 
+const _ = require('lodash');
 const request = require('superagent');
+const cache = require('./cache');
+const key = _.template('currency:<%= from %>:<%= to %>:');
 
 class Currency
 {
-  constructor () {
+  constructor (params) {
+    this.from = params.from;
+    this.to = params.to;
+    this.amount = params.amount;
+  }
+  validate () {
+    let self = this;
+    return new Promise (function (resolve,reject) {
+      if (!self.from || !self.to) {
+        return reject('Malformed query params syntax',400);
+      }
+      resolve();
+    });
+  }
+  fetch () {
+    let self = this;
+    return new Promise (function (resolve,reject) {
+      request
+        .get(process.env.CURRENCY_ADDRESS + '/' + self.from + '/' + self.to)
+        .end(function (err,res) {
+          if (err) {
+            return recovery(resolve,reject);
+          }
+          resolve(res.body);
+          write(res.body);
+        });
+    });
+  }
+  format () {
 
   }
+  write () {
 
+  }
+  recovery () {
+
+  }
 }
 
 module.exports = Currency;
--- models/weather.js
+++ models/weather.js
@@ -1,16 +1,66 @@
 // Created by Isra San Jose Gonzalez
-// @jarvis 01/02/2016
+// @jarvis 07/02/2016
 
 'use strict'
 
+const _ = require('lodash');
 const request = require('superagent');
+const cache = require('./cache');
+const key = _.template('weather:<%= lat %>:<%= lon %>:');
+const config = require('../package.json');
 
 class Weather
 {
-  constructor () {
-
+  constructor (params) {
+    this.lat = params.lat;
+    this.lon = params.lon;
+    this.forecast = params.forecast;
   }
-
+  validate () {
+    let self = this;
+    return new Promise (function (resolve,reject) {
+      if (!self.lat || !self.lon) {
+        return reject('Malformed query params syntax',400);
+      }
+      resolve();
+    });
+  }
+  fetch () {
+    let self = this;
+    return new Promise (function (resolve,reject) {
+      request
+        .get(process.env.WEATHER_ADDRESS + '/forecast/' + self.lat + '/' + self.lon)
+        .end(function (err,res) {
+          if (err) {
+            return recovery(resolve,reject);
+          }
+          resolve(res.body);
+          write(res.body);
+        });
+    });
+  }
+  format (res) {
+    let self = this;
+    return new Promise (function (resolve,reject) {
+      resolve({
+        version:config.version,
+        lat:self.lat,
+        lon:self.lon,
+        weather:res
+      });
+    });
+  }
+  write (data) {
+    cache.set(key({ lat:this.lat,lon:this.lon }),data);
+  }
+  recovery (resolve,reject) {
+    cache.get(key({ lat:this.lat,lon:this.lon }),function (err,res) {
+      if (err || !res) {
+        return reject('Impossible connect to weather service.');
+      }
+      resolve(res);
+    });
+  }
 }
 
 module.exports = Weather;
