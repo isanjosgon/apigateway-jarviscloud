4d2ec911983653be45dd0a56b684eb88ccc775f7
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-02-09 11:19:42 +0100

    Chaining currency methods

--- models/currency.js
+++ models/currency.js
@@ -5,8 +5,9 @@
 
 const _ = require('lodash');
 const request = require('superagent');
-const cache = require('./cache');
+const Cache = require('./cache');
 const key = _.template('currency:<%= from %>:<%= to %>:');
+const config = require('../package.json');
 
 class Currency
 {
@@ -14,38 +15,50 @@ class Currency
     this.from = params.from;
     this.to = params.to;
     this.amount = params.amount;
+    this.cache = new Cache();
   }
   validate () {
-    let self = this;
-    return new Promise (function (resolve,reject) {
-      if (!self.from || !self.to) {
-        return reject('Malformed query params syntax',400);
-      }
-      resolve();
-    });
+    if (!this.from || !this.to) {
+        this.error = 'Malformed query params syntax';
+    }
+    return this;
   }
   fetch () {
     let self = this;
     return new Promise (function (resolve,reject) {
+      if (self.error) {
+        return reject(self.error,400);
+      }
       request
         .get(process.env.CURRENCY_ADDRESS + '/' + self.from + '/' + self.to)
         .end(function (err,res) {
           if (err) {
-            return recovery(resolve,reject);
+            return self.recovery(resolve,reject);
           }
-          resolve(res.body);
-          write(res.body);
+          self.format(res.body,resolve);
+          self.write(res.body);
         });
     });
   }
-  format () {
-
+  format (res,resolve) {
+    resolve({
+      version:config.version,
+      from:this.from,
+      to:this.to,
+      currency:res
+    });
   }
-  write () {
-
+  write (data) {
+    this.cache.set(key({ from:this.from,to:this.to }),data);
   }
-  recovery () {
-
+  recovery (resolve,reject) {
+    let self = this;
+    self.cache.get(key({ from:self.from,to:self.to }),function (err,res) {
+      if (err || !res) {
+        return reject('Impossible connect to currency service.');
+      }
+      self.format(res,resolve);
+    });
   }
 }
 
