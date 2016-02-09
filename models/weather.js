27909866b9cf0c94a6a3d940d8c3c39c213df647
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-02-09 11:14:56 +0100

    Chaining weather methods

--- models/weather.js
+++ models/weather.js
@@ -5,7 +5,7 @@
 
 const _ = require('lodash');
 const request = require('superagent');
-const cache = require('./cache');
+const Cache = require('./cache');
 const key = _.template('weather:<%= lat %>:<%= lon %>:');
 const config = require('../package.json');
 
@@ -15,50 +15,49 @@ class Weather
     this.lat = params.lat;
     this.lon = params.lon;
     this.forecast = params.forecast;
+    this.cache = new Cache();
   }
   validate () {
-    let self = this;
-    return new Promise (function (resolve,reject) {
-      if (!self.lat || !self.lon) {
-        return reject('Malformed query params syntax',400);
-      }
-      resolve();
-    });
+    if (!this.lat || !this.lon) {
+      this.error = 'Malformed query params syntax';
+    }
+    return this;
   }
   fetch () {
-    let self = this;
+    var self = this;
     return new Promise (function (resolve,reject) {
+      if (self.error) {
+        return reject(self.error,400);
+      }
       request
         .get(process.env.WEATHER_ADDRESS + '/forecast/' + self.lat + '/' + self.lon)
         .end(function (err,res) {
           if (err) {
-            return recovery(resolve,reject);
+            return self.recovery(resolve,reject);
           }
-          resolve(res.body);
-          write(res.body);
+          self.format(res.body,resolve);
+          self.write(res.body);
         });
     });
   }
-  format (res) {
-    let self = this;
-    return new Promise (function (resolve,reject) {
-      resolve({
-        version:config.version,
-        lat:self.lat,
-        lon:self.lon,
-        weather:res
-      });
+  format (res,resolve) {
+    resolve({
+      version:config.version,
+      lat:this.lat,
+      lon:this.lon,
+      weather:res
     });
   }
   write (data) {
-    cache.set(key({ lat:this.lat,lon:this.lon }),data);
+    this.cache.set(key({ lat:this.lat,lon:this.lon }),data);
   }
   recovery (resolve,reject) {
-    cache.get(key({ lat:this.lat,lon:this.lon }),function (err,res) {
+    let self = this;
+    self.cache.get(key({ lat:self.lat,lon:self.lon }),function (err,res) {
       if (err || !res) {
         return reject('Impossible connect to weather service.');
       }
-      resolve(res);
+      self.format(res,resolve);
     });
   }
 }
