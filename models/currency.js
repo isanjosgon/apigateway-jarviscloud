201bc9c0d7e985030643eb7bd5d7a56ada57b670
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-02-14 01:41:49 +0100

    Clean code Currency class

--- models/currency.js
+++ models/currency.js
@@ -3,11 +3,7 @@
 
 'use strict'
 
-const _ = require('lodash');
 const request = require('superagent');
-const Cache = require('./cache');
-const key = _.template('currency:<%= from %>:<%= to %>:');
-const config = require('../package.json');
 
 class Currency
 {
@@ -15,11 +11,10 @@ class Currency
     this.from = params.from;
     this.to = params.to;
     this.amount = params.amount;
-    this.cache = new Cache();
   }
   validate () {
     if (!this.from || !this.to) {
-        this.error = 'Malformed query params syntax';
+      this.error = 'Malformed query params syntax';
     }
     return this;
   }
@@ -33,31 +28,10 @@ class Currency
         .get(process.env.CURRENCY_ADDRESS + '/' + self.from + '/' + self.to)
         .end(function (err,res) {
           if (err) {
-            return self.recovery(resolve,reject);
+            return reject('Impossible connect to currency service.');
           }
-          resolve(self.format(res.body));
-          self.save(res.body);
+          resolve(res.body);
         });
-    });
-  }
-  format (res) {
-    return {
-      version:config.version,
-      from:this.from,
-      to:this.to,
-      currency:res
-    };
-  }
-  save (data) {
-    this.cache.set(key({ from:this.from,to:this.to }),data);
-  }
-  recovery (resolve,reject) {
-    let self = this;
-    self.cache.get(key({ from:self.from,to:self.to }),function (err,res) {
-      if (err || !res) {
-        return reject('Impossible connect to currency service.');
-      }
-      resolve(self.format(res));
     });
   }
 }
